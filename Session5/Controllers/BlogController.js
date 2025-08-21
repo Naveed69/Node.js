04607896a132e94d254a8c1578b54d715f5344dc
naveed69 <naveed.desai.69@gmail.com>
2025-08-21 19:10:26 +0530

    Done Crud Oprations on MongoDB

--- Session5/Controllers/BlogController.js
+++ Session5/Controllers/BlogController.js
@@ -1,33 +1,48 @@
 const BlogModel = require("../Models/Blog.model");
 
-async function createBlog(req, res) {
-  const body = req.body;
-  const title = body.title;
-  const content = body.content;
+const createBlog = async (req, res) => {
+  const { title, content } = req.body;
 
   //object creation
-  const blogObj = BlogModel({
-    title,
-    content,
-  });
+  const blogObj = BlogModel({ title, content });
 
   //save to the db
-
   try {
     const response = await blogObj.save();
     res.status(200).json(response);
   } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
+    res.status(500).json({ message: "Cant enter data in DB", err });
   }
-}
-function getAllBlog(req, res) {
-  res.send("All Blog");
-}
-function getBlogById(req, res) {
-  res.send("Blog by ID ");
-}
-function deleteBlogById(req, res) {
-  res.send("Deleted Blog");
-}
+};
+const getAllBlog = async (req, res) => {
+  try {
+    const response = await BlogModel.find({});
+    res.status(200).json(response);
+  } catch (err) {
+    res
+      .status(404)
+      .send({ message: "Could Not Fetch Blogs from DB", error }, err);
+  }
+};
+const getBlogById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await BlogModel.find({ _id: id });
+    if (response.length > 0) res.status(200).json({ message: response });
+    else res.status(404).json({ message: "Data is not available" });
+  } catch (err) {
+    res.status(404).json({ message: "Data is not available", err });
+  }
+};
+const deleteBlogById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const response = await BlogModel.deleteOne({ _id: id });
+    if (response.deletedCount > 0) res.status(200).json({ message: response });
+    else res.status(404).json({ message: "Can't delete at the movement" });
+  } catch (err) {
+    res.status(404).json({ message: "Please provide valid ID" });
+  }
+};
 module.exports = { createBlog, getAllBlog, getBlogById, deleteBlogById };
