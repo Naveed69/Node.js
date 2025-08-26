00f462ec417b7f099c57ce8325f2bc956726367f
naveed69 <naveed.desai.69@gmail.com>
2025-08-26 12:08:53 +0530

    created node.js project

--- Session6/index.js
+++ Session6/index.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello node started");
+});
+app.get("/contact", (req, res) => {
+  res.send("mob: 9008888569");
+});
+app.get("/fitness", (req, res) => {
+  res.send("Hello node started");
+});
+app.get("/services", (req, res) => {
+  res.send("Hello node started");
+});
+
+app.listen(port, () => {
+  console.log("Server started to listen at:", port);
+});
