ca07eaff11f2d54ae4a5f3ce73d43060d43e45a8
naveed69 <naveed.desai.69@gmail.com>
2025-08-28 10:12:39 +0530

    Created db connect and did Crud oprations

--- Session6/Controller/UserSchemaController.js
+++ Session6/Controller/UserSchemaController.js
@@ -1,20 +1,58 @@
 const { user } = require("../Models/userModel");
 
 const addUser = async (req, res) => {
-  //   const { name, age, mobile, branch } = req.body;
-  console.log(req.body);
+  const { name, age, mobile, branch } = req.body;
+  //   console.log(req.body);
   try {
-    // const newuser = user({
-    //   name,
-    //   age,
-    //   mobile,
-    //   branch,
-    // });
-    console.log("newuser");
-    res.send("User added successfully");
+    const newuser = user({
+      name,
+      age,
+      mobile,
+      branch,
+    });
+    const response = await newuser.save();
+    res.status(200).json({ message: "User added successfully", response });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-module.exports = { addUser };
+const GetUserByIdFromDB = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const userData = await user.findById(id);
+    res.status(200).json({ message: "User fetched successfully", userData });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "User Not Found please check id", err: err.message });
+  }
+};
+
+const UpdateUser = async (req, res) => {
+  const id = req.params.id;
+  const { age, branch } = req.body;
+  try {
+    const updatedUser = await user.findByIdAndUpdate(id, { age, branch });
+    res.status(200).json({ message: "User updated successfully", updatedUser });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const deletedUser = await user.findByIdAndDelete(id);
+    res.status(200).json({ message: "user deleted", deletedUser });
+  } catch (err) {
+    res.status(500).json({
+      message: "User Not Found can't able to delete",
+      err: err.message,
+    });
+  }
+};
+
+module.exports = { addUser, GetUserByIdFromDB, UpdateUser, deleteUser };
--- Session6/Routes/UserApiRouter.js
+++ Session6/Routes/UserApiRouter.js
@@ -5,12 +5,21 @@ const {
   SearchByGender,
 } = require("../Controller/UserApiController");
 const { Authorize } = require("../MiddleWare/Authorization");
-const { addUser } = require("../Controller/UserSchemaController");
+const {
+  addUser,
+  GetUserByIdFromDB,
+  UpdateUser,
+  deleteUser,
+} = require("../Controller/UserSchemaController");
 const router = express.Router();
 
 router.get("/allusers", Authorize, GetAllUsers);
 router.get("/user/:id", Authorize, GetUserById);
 router.get("/search", Authorize, SearchByGender);
+//MongoDb routes
 router.post("/adduser", Authorize, addUser);
+router.get("/getuser/:id", Authorize, GetUserByIdFromDB);
+router.put("/updateUser/:id", Authorize, UpdateUser);
+router.delete("/deleteUser/:id", Authorize, deleteUser);
 
 module.exports = router;
