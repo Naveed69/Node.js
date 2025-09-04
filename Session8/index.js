f5e11deefd201db4455ac4d5c41f38df703de90c
naveed69 <naveed.desai.69@gmail.com>
2025-09-04 12:24:56 +0530

    middleware authorization

--- Session8/index.js
+++ Session8/index.js
@@ -3,12 +3,13 @@ const app = express();
 require("dotenv").config();
 const Homerouter = require("./src/routes/HomeRoutes");
 const ApiRouter = require("./src/routes/ApiRoute");
+const Authenticate = require("./src/Middleware/Authentication");
 port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use("/", Homerouter);
-app.use("/api/v1", ApiRouter);
+app.use("/api/v1", Authenticate, ApiRouter);
 
 app.listen(port, () => {
   console.log("server started at port: ", port);
