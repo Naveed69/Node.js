393d436f82d9d246f041ac51d4f1b7a5b5d6f99f
naveed69 <naveed.desai.69@gmail.com>
2025-09-09 12:19:34 +0530

    added env file

--- Session-8-mern-2-takehomes copy/disfo-Naveed/src/MiddleWare/Authorization.js
+++
@@ -1,9 +0,0 @@
-const Authorize = (req, res, next) => {
-  if (process.env.Auth_SECRET_KEY === req.headers.authorization) {
-    next();
-  } else {
-    res.status(403).json({ message: "Unauthorised Access" });
-  }
-};
-
-module.exports = Authorize;
