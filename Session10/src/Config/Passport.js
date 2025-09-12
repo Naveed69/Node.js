8aec71fa4926f9a2a9d965a7aa388069b5e5c612
naveed69 <naveed.desai.69@gmail.com>
2025-09-12 11:36:53 +0530

    added jwt passport

---
+++ Session10/src/Config/Passport.js
@@ -0,0 +1,22 @@
+const { JWT_KEY } = require("../Middleware/Authentication");
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+var options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWT_KEY,
+};
+
+const jwtStrategy = new JwtStrategy(options, (payload, done) => {
+  try {
+    console.log("Startergy payload", payload);
+    return done(null, true);
+  } catch (err) {
+    return done(err, false);
+  }
+});
+
+module.exports = (passport) => {
+  passport.use(jwtStrategy);
+};
