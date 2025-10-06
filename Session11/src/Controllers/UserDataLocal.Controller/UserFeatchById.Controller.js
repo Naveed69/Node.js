79e929e6d57238dbdd194d6b0bfcc8c792c7fb77
naveed69 <naveed.desai.69@gmail.com>
2025-10-06 20:49:50 +0530

    created new session

--- Session11/src/Controllers/UserDataLocal.Controller/UserFeatchById.Controller.js
+++ Session11/src/Controllers/UserDataLocal.Controller/UserFeatchById.Controller.js
@@ -2,8 +2,13 @@ const userData = require("../../../userData");
 
 const getUsersById = (req, res) => {
   const id = req.params.id;
-  const user = userData.data.filter((user) => user.id === id);
-  res.send(user);
+  console.log(id);
+  const user = userData.data.find((user) => user.id === Number(id));
+  if (user) {
+    res.send(user); // Send the user if found
+  } else {
+    res.status(404).send({ message: "User not found" }); // Return a 404 if no user is found
+  }
 };
 
 module.exports = { getUsersById };
