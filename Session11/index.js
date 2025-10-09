85bb81852898dfeb33f248581615b9983763ad6d
naveed69 <naveed.desai.69@gmail.com>
2025-10-09 09:37:17 +0530

    conected fronend counter to this backend

--- Session11/index.js
+++ Session11/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const UserRoutes = require("./src/Routers/Users.Router");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
-
+app.use(cors({origin: 'http://localhost:3000',}));
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
