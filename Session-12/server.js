c972818f69f2e31b303de9230904a979e11d4d43
naveed69 <naveed.desai.69@gmail.com>
2025-10-30 13:00:31 +0530

    fixed Session-12 folder (removed embedded repo)

---
+++ Session-12/server.js
@@ -0,0 +1,40 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/tasksdb");
+
+const Task = mongoose.model("Task", {
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+// app.get("/tasks", async (req, res) => {
+//     res.send("Get all tasks");
+// });
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+
+app.listen(5000, () => console.log("Server running at port :5000"));
